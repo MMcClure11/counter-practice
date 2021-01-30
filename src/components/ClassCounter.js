d16cad0f22093516a86b892c08be07de23070432
MMcClure11 <mcclme02@luther.edu>
2021-01-30 17:36:51 -0600

    Add state and increment to Class COUnter

--- src/components/ClassCounter.js
+++ src/components/ClassCounter.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 
 export default class ClassCounter extends Component {
+
+  state = {
+    count: 0,
+  }
+
+  increment = () => {
+    this.setState(
+      prevState => ({count: prevState.count + 1})
+    )
+  }
+
   render() {
     return (
       <div>
         <h2>I am a counter using a Class Component</h2>
+        { this.state.count }
+        <button onClick={ this.increment }>Increment</button>
       </div>
     )
   }
