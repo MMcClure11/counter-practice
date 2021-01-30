e3bcb7b1dc6cf907d98e2890a83d3536b7cadfee
MMcClure11 <mcclme02@luther.edu>
2021-01-30 16:47:12 -0600

    Add increment to hook

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -2,10 +2,17 @@ import React, { useState } from 'react'
 
 const HookCounter = () => {
   const [ counter, setCounter ] = useState(0)
+
+  const increment = () => {
+    return setCounter(prevCount => prevCount + 1)
+  }
+
   return(
     <div>
       <h2>I am a counter built using hooks</h2>
       { counter }
+      <button onClick={ increment }>Increment</button>
+      <button>Decrement</button>
     </div>
   )
 }
