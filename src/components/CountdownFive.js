4e64dede6a82037361e3db2b3561ce85ae0520f7
MMcClure11 <mcclme02@luther.edu>
2021-02-01 11:50:31 -0600

    Add componentDidMount with setInterval

--- src/components/CountdownFive.js
+++ src/components/CountdownFive.js
@@ -7,11 +7,26 @@ export default class CountDownFive extends Component {
     seconds: 0
   }
 
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      if( this.state.seconds <= 0 && this.state.minutes !== 0 ){
+        this.setState((prevState) => ({
+          minutes: prevState.minutes - 1,
+          seconds: 59
+        }))
+      } else {
+        this.setState((prevState) => ({
+          seconds: prevState.seconds - 1
+        }))
+      }
+    }, 1000)
+  }
+
   render(){
     const { minutes, seconds } = this.state
     return(
       <div>
-        <h1>Time Remaining: { minutes } : { seconds < 10 ? `0${seconds}` : seconds }</h1>
+        <h1>Time Remaining - { minutes } : { seconds < 10 ? `0${seconds}` : seconds }</h1>
       </div>
     )
   }
