3318c58b2dc62cafa61de0e4cc88fd266fc87aac
MMcClure11 <mcclme02@luther.edu>
2021-02-01 10:56:18 -0600

    Add three minute countdown

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -8,12 +8,32 @@ export default class CountDown extends Component {
     seconds: 0
   }
 
+  componentDidMount() {
+    this.inverval = setInterval(() => {
+      let seconds = this.state.seconds - 1
+      let minutes = this.state.minutes - 1
+      if ( seconds >= 0 ) {
+        this.setState({
+          seconds: seconds
+        })
+      } else {
+        this.setState({
+          minutes: minutes,
+          seconds: 59
+        })
+      }
+      if ( minutes === 0 ) {
+        clearInterval(this.inverval)
+      }
+    }, 1000)
+  }
+
  render(){
    const { count, minutes, seconds } = this.state
    return(
-    <div>
+    <>
       <h1>Time Remaining: { minutes } minutes, { seconds } seconds</h1>     
-    </div>
+    </>
    )
  }
 }
