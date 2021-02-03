f8626efb001d710c6d515e4f99d360a5ea6aeec3
MMcClure11 <mcclme02@luther.edu>
2021-02-03 10:50:31 -0600

    Add and pass test for counter starts at 0

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -31,7 +31,10 @@ const HookCounter = () => {
   return(
     <div data-test='hook-counter-component'>
       <h2>I am a counter built using hooks</h2>
-      <h3 data-test='counter-display'>{ counter }</h3>
+      <h3 data-test='counter-display'>
+        Count is at:&nbsp;
+        <span data-test='count'>{ counter }</span>
+      </h3>
       <button onClick={ reset }>Reset</button>
       <button onClick={ increment }>Increment</button>
       <button onClick={ decrement }>Decrement</button>
